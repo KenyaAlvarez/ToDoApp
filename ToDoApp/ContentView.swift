087a0b5295a5cd1771eeab9e12adb5ed8aa6550a
Arturo Sánchez <96492801+KenyaKateAlvarezBaeza@users.noreply.github.com>
2023-03-28 18:06:50 -0600

    Fisrt project
    first commit.

---
+++ ToDoApp/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  ToDoApp
+//
+//  Created by Arturo Sanchez on 28/03/23.
+//
+
+import SwiftUI
+
+struct TodoItem: Identifiable {
+    let id = UUID()
+    let task: String
+}
+
+struct TodoList: View {
+    @State var items = [TodoItem]()
+    @State var newTask = ""
+    
+    var body: some View{
+        VStack {
+            Text("ToDo List")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.top, 50)
+                .foregroundColor(Color.red.opacity(0.7))
+            
+            HStack{
+                TextField("New task", text: $newTask)
+                    .padding(.all)
+                    .background(Color.pink.opacity(0.4))
+                    .cornerRadius(25)
+                    .accentColor(.red)
+                Button(action: addTask){
+                    Text("Add")
+                        .foregroundColor(Color.red.opacity(0.7))
+                }
+            }
+            .padding()
+            
+            VStack{
+                List(items) { item in
+                    HStack{
+                        Text(item.task)
+                            .foregroundColor(Color.red)
+                        Spacer()
+                        Button(action: {removeTask(item) }) {
+                            Image(systemName: "trash")
+                                .foregroundColor(.red)
+                        }
+                        
+                    }
+                }
+            }
+            
+        }
+        
+        .background(Color.red.opacity(0.3))
+    }
+    
+    func addTask(){
+        if !newTask.isEmpty {
+            items.append(TodoItem(task: newTask))
+            newTask = ""
+        }
+    }
+    
+    func removeTask(_ item: TodoItem) {
+        items.removeAll{ $0.id == item.id}
+    }
+}
+
+struct ContentView: View{
+    var body: some View{
+        TodoList()
+    }
+}
+
+struct ContentView_Previews : PreviewProvider{
+    static var previews: some View{
+        ContentView()
+    }
+    
+}
---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Arturo Sanchez on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+}
